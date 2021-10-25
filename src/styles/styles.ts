88fa94646b80a88e0ad460e80c41a0661353128b
Guled Ahmed <akindnesstoremember@gmail.com>
2021-10-24 21:46:52 -0700

    making changes to the background color and paragraph font colors

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -27,7 +27,7 @@ export const Styles = createGlobalStyle`
         padding:0;
         border: 0;
         outline: 0;
-        background: black;
+        background: tan;
         overflow-x: hidden;
     }
 
@@ -72,7 +72,7 @@ export const Styles = createGlobalStyle`
     }
 
     p {
-        color: white;
+        color: black;
         font-size: 21px;        
         line-height: 1.41;
     }
